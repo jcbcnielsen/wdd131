46d4bde0c130296dcde035bc98c481dfb29b323e
jcbcnielsen <jcbcnielsen@proton.me>
2025-05-21 16:48:15 -0400

    Week 2 Edit B

--- scripts/place.js
+++ scripts/place.js
@@ -8,8 +8,7 @@ let cond = "Overcast";
 let windvel = 5;
 
 function calculateWindChill(t, v) {
-    let chill = 13.12 + (0.6215 * t) - (11.37 * (v ** 0.16)) + (0.3965 * t * (v ** 0.16));
-    return chill;
+    return 13.12 + (0.6215 * t) - (11.37 * (v ** 0.16)) + (0.3965 * t * (v ** 0.16));
 }
 
 tempcell.textContent = `${temp} °C`;
@@ -17,8 +16,7 @@ condcell.textContent = cond;
 speedcell.textContent = `${windvel} km/h`;
 
 if (temp <= 10 && windvel > 4.8) {
-    let windchill = calculateWindChill(temp, windvel).toFixed(2);
-    chillcell.textContent = `${windchill} °C`;
+    chillcell.textContent = `${calculateWindChill(temp, windvel).toFixed(2)} °C`;
 }
 else {
     chillcell.textContent = `N/A`;
